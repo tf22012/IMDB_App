4242fa18f7c6a10db2da2673b17834e55da9c057
Chris Charles <w0481147@nscc.ca>
2024-12-11 11:53:25 -0400

    Cleaned up code

--- Pages/ActorsPage.xaml.cs
+++ Pages/ActorsPage.xaml.cs
@@ -33,8 +33,6 @@ namespace IMDB_App.Pages
         public ActorsPage()
         {
             InitializeComponent();
-            //CreateAlphabetButtons();
-            //directors = GetDirectorsFromDatabase(); // Fetch the directors
             //Tie the markup viewsource object to the C# code viewsource object
             actorsViewSource = (CollectionViewSource)FindResource(nameof(actorsViewSource));
 
@@ -59,21 +57,13 @@ namespace IMDB_App.Pages
                  select new
                  {
                      HeaderText = $"{actorGroup.Key}",
-                     actors = actorGroup.Take(5).ToList()
+                     actors = actorGroup.Take(10).ToList()
                  })
-                .Take(5);
+                .Take(10);
 
 
             // Execute the query against the database and assign it as the data source for the list view
             actorsListView.ItemsSource = query.ToList();
-
-            //string query = @"
-            //    SELECT n.primaryName
-            //    FROM dbo.Directors d
-            //    INNER JOIN dbo.Names n
-            //        ON d.nameID = n.nameID
-            //    WHERE n.primaryProfession LIKE '%director%'
-            //    ORDER BY n.primaryName";
         }
 
         private void SearchBtn_Click(object sender, RoutedEventArgs e)
@@ -81,115 +71,8 @@ namespace IMDB_App.Pages
             // Searchs when the button is clicked
             var searchText = actorsSearchBox.Text.ToLower();
 
-            //var filteredDirectors
             LoadActorData(searchText);
-            //string searchQuery = catalogSearchBox.Text;
-
-            //if (string.IsNullOrEmpty(searchQuery))
-            //{
-            //    MessageBox.Show("Please enter a search query.");
-            //    return;
-            //}
-
-            //var filteredDirectors = directors.Where(d => d.Contains(searchQuery, StringComparison.OrdinalIgnoreCase)).ToList();
-
-            //if (filteredDirectors.Any())
-            //{
-            //    DirectorsListBox.ItemsSource = filteredDirectors;
-            //}
-            //else
-            //{
-            //    MessageBox.Show($"No directors found for '{searchQuery}'.");
-            //}
         }
 
-        //// Create alphabet buttons from A-Z
-        //private void CreateAlphabetButtons()
-        //{
-        //    for (char letter = 'A'; letter <= 'Z'; letter++)
-        //    {
-        //        Button alphabetButton = new Button
-        //        {
-        //            Content = letter.ToString(),
-        //            Width = 35,
-        //            Height = 35,
-        //            Margin = new Thickness(5),
-        //            Background = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(159, 173, 189)),
-        //            Foreground = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(0, 0, 0)),
-        //            FontSize = 18
-        //        };
-
-        //        alphabetButton.Click += AlphabetButton_Click;
-
-        //        AlphabetPanel.Children.Add(alphabetButton);
-        //    }
-        //}
-
-        //private void AlphabetButton_Click(object sender, RoutedEventArgs e)
-        //{
-        //    Button clickedButton = sender as Button;
-        //    string selectedLetter = clickedButton.Content.ToString();
-        //    ShowDirectorsDropdown(selectedLetter);
-        //}
-
-        //// Show a dropdown of directors whose first name starts with the selected letter
-        //private void ShowDirectorsDropdown(string letter)
-        //{
-        //    // Filter directors based on the selected letter
-        //    var filteredDirectors = directors.Where(d => d.StartsWith(letter, StringComparison.OrdinalIgnoreCase)).ToList();
-
-        //    if (filteredDirectors.Count > 0)
-        //    {
-        //        ComboBox directorComboBox = new ComboBox
-        //        {
-        //            Width = 200,
-        //            Height = 30,
-        //            Margin = new Thickness(5),
-        //            ItemsSource = filteredDirectors
-        //        };
-
-        //        AlphabetPanel.Children.Add(directorComboBox);
-        //    }
-        //    else
-        //    {
-        //        MessageBox.Show($"No directors found with the first name starting with '{letter}'.");
-        //    }
-        //}
-
-        //// Get directors from the database
-        //private List<string> GetDirectorsFromDatabase()
-        //{
-        //    List<string> directors = new List<string>();
-        //    string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=IMDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
-
-        //    string query = @"
-        //        SELECT n.primaryName
-        //        FROM dbo.Directors d
-        //        INNER JOIN dbo.Names n
-        //            ON d.nameID = n.nameID
-        //        WHERE n.primaryProfession LIKE '%director%'
-        //        ORDER BY n.primaryName";
-
-        //    using (SqlConnection connection = new SqlConnection(connectionString))
-        //    {
-        //        SqlCommand command = new SqlCommand(query, connection);
-
-        //        try
-        //        {
-        //            connection.Open();
-        //            SqlDataReader reader = command.ExecuteReader();
-        //            while (reader.Read())
-        //            {
-        //                directors.Add(reader["primaryName"].ToString());
-        //            }
-        //            reader.Close();
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            MessageBox.Show($"Error fetching directors: {ex.Message}");
-        //        }
-        //    }
-        //    return directors;
-        //}
     }
 }
--- Pages/DirectorsPage.xaml.cs
+++ Pages/DirectorsPage.xaml.cs
@@ -31,8 +31,7 @@ namespace IMDB_App.Pages
         public DirectorsPage()
         {
             InitializeComponent();
-            //CreateAlphabetButtons();
-            //directors = GetDirectorsFromDatabase(); // Fetch the directors
+
             //Tie the markup viewsource object to the C# code viewsource object
             directorsViewSource = (CollectionViewSource)FindResource(nameof(directorsViewSource));
 
@@ -65,13 +64,6 @@ namespace IMDB_App.Pages
             // Execute the query against the database and assign it as the data source for the list view
             directorsListView.ItemsSource = query.ToList();
 
-            //string query = @"
-            //    SELECT n.primaryName
-            //    FROM dbo.Directors d
-            //    INNER JOIN dbo.Names n
-            //        ON d.nameID = n.nameID
-            //    WHERE n.primaryProfession LIKE '%director%'
-            //    ORDER BY n.primaryName";
         }
 
         private void SearchBtn_Click(object sender, RoutedEventArgs e)
@@ -79,115 +71,8 @@ namespace IMDB_App.Pages
             // Searchs when the button is clicked
             var searchText = directorsSearchBox.Text.ToLower();
 
-            //var filteredDirectors
             LoadDirectorData(searchText);
-            //string searchQuery = catalogSearchBox.Text;
-
-            //if (string.IsNullOrEmpty(searchQuery))
-            //{
-            //    MessageBox.Show("Please enter a search query.");
-            //    return;
-            //}
-
-            //var filteredDirectors = directors.Where(d => d.Contains(searchQuery, StringComparison.OrdinalIgnoreCase)).ToList();
-
-            //if (filteredDirectors.Any())
-            //{
-            //    DirectorsListBox.ItemsSource = filteredDirectors;
-            //}
-            //else
-            //{
-            //    MessageBox.Show($"No directors found for '{searchQuery}'.");
-            //}
         }
 
-        //// Create alphabet buttons from A-Z
-        //private void CreateAlphabetButtons()
-        //{
-        //    for (char letter = 'A'; letter <= 'Z'; letter++)
-        //    {
-        //        Button alphabetButton = new Button
-        //        {
-        //            Content = letter.ToString(),
-        //            Width = 35,
-        //            Height = 35,
-        //            Margin = new Thickness(5),
-        //            Background = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(159, 173, 189)),
-        //            Foreground = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(0, 0, 0)),
-        //            FontSize = 18
-        //        };
-
-        //        alphabetButton.Click += AlphabetButton_Click;
-
-        //        AlphabetPanel.Children.Add(alphabetButton);
-        //    }
-        //}
-
-        //private void AlphabetButton_Click(object sender, RoutedEventArgs e)
-        //{
-        //    Button clickedButton = sender as Button;
-        //    string selectedLetter = clickedButton.Content.ToString();
-        //    ShowDirectorsDropdown(selectedLetter);
-        //}
-
-        //// Show a dropdown of directors whose first name starts with the selected letter
-        //private void ShowDirectorsDropdown(string letter)
-        //{
-        //    // Filter directors based on the selected letter
-        //    var filteredDirectors = directors.Where(d => d.StartsWith(letter, StringComparison.OrdinalIgnoreCase)).ToList();
-
-        //    if (filteredDirectors.Count > 0)
-        //    {
-        //        ComboBox directorComboBox = new ComboBox
-        //        {
-        //            Width = 200,
-        //            Height = 30,
-        //            Margin = new Thickness(5),
-        //            ItemsSource = filteredDirectors
-        //        };
-
-        //        AlphabetPanel.Children.Add(directorComboBox);
-        //    }
-        //    else
-        //    {
-        //        MessageBox.Show($"No directors found with the first name starting with '{letter}'.");
-        //    }
-        //}
-
-        //// Get directors from the database
-        //private List<string> GetDirectorsFromDatabase()
-        //{
-        //    List<string> directors = new List<string>();
-        //    string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=IMDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
-
-        //    string query = @"
-        //        SELECT n.primaryName
-        //        FROM dbo.Directors d
-        //        INNER JOIN dbo.Names n
-        //            ON d.nameID = n.nameID
-        //        WHERE n.primaryProfession LIKE '%director%'
-        //        ORDER BY n.primaryName";
-
-        //    using (SqlConnection connection = new SqlConnection(connectionString))
-        //    {
-        //        SqlCommand command = new SqlCommand(query, connection);
-
-        //        try
-        //        {
-        //            connection.Open();
-        //            SqlDataReader reader = command.ExecuteReader();
-        //            while (reader.Read())
-        //            {
-        //                directors.Add(reader["primaryName"].ToString());
-        //            }
-        //            reader.Close();
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            MessageBox.Show($"Error fetching directors: {ex.Message}");
-        //        }
-        //    }
-        //    return directors;
-        //}
     }
 }
